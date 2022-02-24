6aeb6016845dad61d13e82c957668f4710e0b5f7
Arundhathi R Menon <43034600+arundhathi6@users.noreply.github.com>
2022-02-24 16:17:13 +0530

    cmt

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const data = require("./data.json");
 let port = process.env.PORT || 5234
+// app.get("/",(req,res)=>{
+//     res.send("Hello")
+    
+// })
 app.get("/",(req,res)=>{
-    res.send("Hello")
-    
-})
-app.get("/products",(req,res)=>{
     res.send(data)
-    
+
 })
 app.listen(port,()=>{
     console.log(`listening..port http://localhost:${port}`)
